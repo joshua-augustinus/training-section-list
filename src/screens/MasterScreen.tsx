05f2bea36bb65195da9e6250de39a3d17149997d
joshua-augustinus <joshua.augustinus@gmail.com>
2020-11-23 15:33:36 +1100

    Working scroll

--- src/screens/MasterScreen.tsx
+++ src/screens/MasterScreen.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
-import { Button, Text, TextInput, TouchableOpacity, View, BackHandler } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { Button, Text, TextInput, TouchableOpacity, View, BackHandler, SectionList, StyleSheet, Animated } from 'react-native';
 import { SafeAreaView, StackActions } from 'react-navigation';
 import { DrawerActions, NavigationDrawerProp } from 'react-navigation-drawer';
 import Icon from 'react-native-vector-icons/Entypo';
+import { FlatList } from 'react-native-gesture-handler';
 
 /**
  * https://reactnavigation.org/docs/4.x/typescript
@@ -11,45 +12,112 @@ type Props = {
     navigation: NavigationDrawerProp<{ userId: string, routeName: string }>;
 }
 
+
+const DATA = [
+    {
+        title: "Main dishes",
+    }
+];
+
+const yPositions = [];
+
+interface ItemProps {
+    title: string,
+    onPress: () => void,
+    index: number
+}
+
+
+const POSITION_BOTTOM = 1;
+
 const MasterScreen = (props: Props) => {
+    const [data, setData] = useState(DATA);
+    const dynamicValue = useRef(new Animated.Value(0)).current;
+    const flatListRef = useRef<any>(null);
+    const yRef = useRef(0);
 
     useEffect(() => {
+        dynamicValue.addListener(({ value }) => {
+            const params = {
+                animated: false,
+                offset: value,
+            }
+            flatListRef.current.scrollToOffset(params)
+        });
+    }, []);
+
+    useEffect(() => {
+        Animated.timing(dynamicValue, {
+            toValue: 1000,
+            duration: 3000,
+            useNativeDriver: true,
+            delay: 100
+        }).start();
 
     }, []);
 
-    const onMenuPress = () => {
-        console.log(props.navigation.state);// { key: 'Home', routeName: 'Home' }
-        console.log("Menu pressed");
-        props.navigation.dispatch(DrawerActions.toggleDrawer());
-    }
 
-    const onButtonPress = () => {
-        const pushAction = StackActions.push({
-            routeName: 'Stack1',
-            params: {
-                myUserId: 9,
-            },
-        });
+    const Item = React.useCallback((props: ItemProps) => (
+        <TouchableOpacity onPress={props.onPress}>
+            <View style={styles.item} onLayout={(event) => {
+                const layout = event.nativeEvent.layout;
+                console.log(layout);
+                yPositions[props.index] = layout;
+            }}>
+                <Text style={styles.title}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
+            </View>
+        </TouchableOpacity>
 
-        props.navigation.dispatch(pushAction);
+    ), []);
+
+    const onButtonPress = React.useCallback(() => {
+        const newMessage = [{
+            title: "Desserts",
+        }]
+        const newData = data.concat(newMessage);
+        setData(newData);
+
+        setTimeout(() => {
+            let totalHeight = 0;
+            for (let i = 0; i < yPositions.length - 1; i++) {
+                totalHeight += yPositions[i].height;
+            }
+
+
+            const ani1 = Animated.timing(dynamicValue, {
+                toValue: yRef.current,
+                duration: 0,
+                useNativeDriver: true,
+                delay: 0
+            })
+
+            const ani2 = Animated.timing(dynamicValue, {
+                toValue: totalHeight,
+                duration: 600,
+                useNativeDriver: true,
+                delay: 0
+            });
+
+            Animated.sequence([ani1, ani2]).start()
+        }, 500);
+
+
+
+    }, [data]);
+
+    const onScroll = ({ nativeEvent }) => {
+        yRef.current = nativeEvent.contentOffset.y;
     }
 
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
-            <View style={{ height: 50, backgroundColor: 'red', flexDirection: 'row', alignItems: 'center' }}>
+            <FlatList ref={flatListRef} inverted={false} onScroll={onScroll}
+                data={data}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item, index }) => <Item index={index} title={item.title} onPress={onButtonPress} />}
 
-                <TouchableOpacity style={{ backgroundColor: 'yellow' }}
-                    onPress={() => onMenuPress()}>
-                    <Text>Menu</Text>
-                </TouchableOpacity>
-            </View>
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Icon name="controller-play" size={24} />
-                <Text>{props.navigation.state.routeName}</Text>
-                <TextInput placeholder="Enter text here..."></TextInput>
-                <Button title="Press me" onPress={() => onButtonPress()}></Button>
-            </View>
+            />
         </SafeAreaView>
 
     );
@@ -59,3 +127,19 @@ const MasterScreen = (props: Props) => {
 MasterScreen.navigationOptions = {}
 
 export { MasterScreen }
+
+const styles = StyleSheet.create({
+
+    item: {
+        backgroundColor: "#f9c2ff",
+        padding: 20,
+        marginVertical: 8
+    },
+    header: {
+        fontSize: 32,
+        backgroundColor: "#fff"
+    },
+    title: {
+        fontSize: 24
+    }
+});
